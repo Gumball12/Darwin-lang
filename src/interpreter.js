4bb82d5c67be04f2560361fd3145d6c3a40ffc2d
HaeJun Seo <kan02134@gmail.com>
2019-06-15 20:03:09 +0900

    Feat: Add pre-defined functions (library)
    and Fix output

--- src/interpreter.js
+++ src/interpreter.js
@@ -16,12 +16,12 @@ define(() => {
     lambda (input, context) {
       return (...args) => { // invoke lambda arguments
         const scope = input[0].reduce((r, x, i) => { // create a new scope (lambda)
-          r[x.value] = args[i];
+          r[x.value] = args[i]; // set variables into the lambda scope
           return r;
         }, { });
   
-        return interpret(input[1], new Context(scope, context));
-      };
+        return interpret(input[1], new Context(scope, context)); // invoke function
+      }; // return lambda function
     },
 
     /**
@@ -31,23 +31,24 @@ define(() => {
      * @param {Context} context context
      */
     set (input, context) {
-      if (input[1] instanceof Array && input[1][input.length - 1].value === 'lambda') { // lambda
-        const lambda = (...args) => { // invoke lambda args
-          const scope = input[1][0].reduce((r, x, i) => { // create a new lambda scope
-            r[x.value] = args[i];
-            return r;
-          }, { });
-
-          return interpret(input[1][1], new Context(scope, context));
-        };
-
-        context.scope[input[0].value] = lambda;
-
-        return lambda;
-      } else { // literals
-        context.scope[input[0].value] = input[1].value;
-        return input[1].value;
+      if (input[1] instanceof Array && input[1][input.length - 1].value === 'lambda') { // set lambda
+        context.scope[input[0].value] = special.lambda(input[1], context); // into the context scope
+        return '/* lambda */';
+      } else { // set literal
+        const setValue = input[1].value;
+        context.scope[input[0].value] = setValue; // into the context scope
+        return setValue;
       }
+    },
+
+    /**
+     * conditional (true-only)
+     * 
+     * @param {[Object]} input parsing array
+     * @param {Context} context context
+     */
+    cond (input, context) {
+      return interpret(input[0], context) ? interpret(input[1], context) : undefined;
     }
   };
   
@@ -58,11 +59,138 @@ define(() => {
     /**
      * object to the console
      * 
-     * @param {*} input write object
+     * @param {*} input 
      */
     print (input) {
       console.log(input);
       return input;
+    },
+    
+    /**
+     * and operation
+     * 
+     * @param  {...any} inputs 
+     */
+    and (...inputs) {
+      return Number(inputs.reduce((r, v) => r && Boolean(v), inputs[0]));
+    },
+
+    /**
+     * or operation
+     * 
+     * @param  {...any} inputs 
+     */
+    or (...inputs) {
+      return Number(inputs.reduce((r, v) => r || Boolean(v), inputs[0]));
+    },
+
+    /**
+     * equal operation
+     * 
+     * @param  {...any} inputs 
+     */
+    eq (...inputs) {
+      return Number(inputs.reduce((r, v) => {
+        r.isEqual = r.value === v;
+        return r;
+      }, { isEqual: true, value: inputs[0] }).isEqual);
+    },
+
+    /**
+     * not operation
+     * 
+     * @param {*} input 
+     */
+    not (input) {
+      return Number(!input)
+    },
+
+    /**
+     * lt(<) operation
+     * 
+     * @param {*} a 
+     * @param {*} b 
+     */
+    lt (a, b) {
+      return Number(a < b);
+    },
+
+    /**
+     * gt(>) operation
+     * 
+     * @param {*} a 
+     * @param {*} b 
+     */
+    gt (a, b) {
+      return Number(a > b);
+    },
+
+    /**
+     * le(<=) operation
+     * 
+     * @param {*} a 
+     * @param {*} b 
+     */
+    le (a, b) {
+      return Number(a <= b);
+    },
+
+    /**
+     * ge(>=) operation
+     * 
+     * @param {*} a 
+     * @param {*} b 
+     */
+    ge (a, b) {
+      return Number(a >= b);
+    },
+
+    /**
+     * add operation
+     * 
+     * @param  {...any} inputs 
+     */
+    add (...inputs) {
+      return inputs.reduce((r, v) => r + v, 0);
+    },
+
+    /**
+     * sub operation
+     * 
+     * @param {*} a first operand
+     * @param {*} b second operand
+     */
+    sub (a, b) {
+      return a - b;
+    },
+
+    /**
+     * mul operation
+     * 
+     * @param  {...any} inputs 
+     */
+    mul (...inputs) {
+      return inputs.reduce((r, v) => r * v, 1);
+    },
+
+    /**
+     * div operation (a / b)
+     * 
+     * @param {*} a first operand
+     * @param {*} b second operand
+     */
+    div (a, b) {
+      return a / b;
+    },
+
+    /**
+     * mod operation (a % b)
+     * 
+     * @param {*} a first operand
+     * @param {*} b second operand
+     */
+    mod (a, b) {
+      return a % b;
     }
   };
   
