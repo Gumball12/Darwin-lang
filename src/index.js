b8ca2fd543dc411099bd66d80fce9ef86b8b6401
HaeJun Seo <kan02134@gmail.com>
2019-07-17 13:58:29 +0800

    Feat: Update REPL for mobile

--- src/index.js
+++ src/index.js
@@ -25,48 +25,40 @@ require(['./Darwin.js'], ({ Darwin }) => {
   const history = ['']; // code history
   let historyIndex = 0;
 
-  let code = ''; // store inputted code
-  let index = code.length; // input code length
-  let currentCodeElement = getCodeElement(parent, 'code');
+  const inputEl = document.createElement('input'); // input DOM element
+
+  let currentCodeElement = getCodeElement(parent, 'code', 'p');
   currentCodeElement.classList.add('active');
+  currentCodeElement.appendChild(inputEl);
 
-  // init elements
-  updateCodeElement(currentCodeElement, code, index);
+  // focusing input element
+  inputEl.focus();
 
-  // keydown handler
-  window.addEventListener('keydown', (evt) => {
-    evt.preventDefault();
+  // handle events
+  inputEl.addEventListener('keydown', ({ key }) => {
+    if (key === 'Enter') { // invoke code
+      // get code
+      const code = inputEl.value;
 
-    const { key } = evt;
-
-    if (key.length === 1) { // insert codes
-      code = code.slice(0, index) + key + code.slice(index);
-      index++;
-    } else if (key === 'Backspace' && index > 0) { // backspace
-      code = code.slice(0, index - 1) + code.slice(index);
-      index--;
-    } else if (key === 'Delete') { // delete
-      code = code.slice(0, index) + code.slice(index + 1);
-    } else if (key === 'Enter') { // invoke code
       // invoke code and append output element
-      const output = getCodeElement(parent, 'output');
+      const output = getCodeElement(parent, 'output', 'p');
       output.innerHTML = _.flowRight(
         arr => arr.join(' '),
         _.partial(_.filter, _, el => el !== undefined),
-        el => {
-          if (el instanceof Array) {
-            return _.flattenDeep(el);
-          } else {
-            return [el];
-          }
-        },
-        Darwin
+        el => el instanceof Array ? _.flattenDeep(el) : [el],
+        Darwin // 🐟
       )(code);
 
       // init current code element
       currentCodeElement.classList.remove('active');
-      currentCodeElement = getCodeElement(parent, 'code');
+      currentCodeElement.removeChild(currentCodeElement.lastChild);
+      currentCodeElement.innerText = code;
+
+      inputEl.value = '';
+
+      currentCodeElement = getCodeElement(parent, 'code', 'p');
       currentCodeElement.classList.add('active');
+      currentCodeElement.appendChild(inputEl);
 
       // history
       if (code !== '') {
@@ -74,29 +66,16 @@ require(['./Darwin.js'], ({ Darwin }) => {
         historyIndex = history.length - 1;
       }
 
-      // init variables
-      code = '';
-      index = 0;
-
       // scroll to bottom
       window.scrollTo(0, document.querySelector('p.code.active').offsetTop);
-    } else if (key === 'ArrowLeft' && index > 0) { // to left
-      index--;
-    } else if (key === 'ArrowRight' && index < code.length) { // to right
-      index++;
-    } else if (key === 'ArrowUp' && historyIndex > 0) {
-      code = history[--historyIndex];
-      index = code.length;
-    } else if (key === 'ArrowDown' && historyIndex < history.length - 1) {
-      code = history[++historyIndex];
-      index = code.length;
-    } else if (key === 'End') {
-      index = code.length;
-    } else if (key === 'Home') {
-      index = 0;
-    }
 
-    updateCodeElement(currentCodeElement, code, index);
+      // focusing input el
+      inputEl.focus();
+    } else if (key === 'ArrowUp' && historyIndex > 0) { // find history
+      inputEl.value = history[--historyIndex];
+    } else if (key === 'ArrowDown' && historyIndex < history.length - 1) { // find history
+      inputEl.value = history[++historyIndex];
+    }
   });
 
   // scroll handler
@@ -111,26 +90,16 @@ require(['./Darwin.js'], ({ Darwin }) => {
   /* tools */
 
   /**
-    * get a new code element(p)
+    * get a new dom element
     * 
     * @param {HTMLElement} parent parent DOM element
     * @param {String} className css class name
+    * @param {String} nodeName node element name (default: 'p')
     */
-  function getCodeElement (parent, className) {
-    const el = document.createElement('p');
+  function getCodeElement (parent, className, nodeName = 'p') {
+    const el = document.createElement(nodeName);
     el.classList.add(className);
     parent.appendChild(el);
     return el;
-  }
-
-  /**
-    * update current code element
-    * 
-    * @param {HTMLElement} codeElement current code element
-    * @param {String} code
-    * @param {Number} index
-    */
-  function updateCodeElement (codeElement, code, index) {
-    codeElement.innerHTML = `${code.slice(0, index)}<span class="bar"></span>${code.slice(index)}`;
   }
 });
